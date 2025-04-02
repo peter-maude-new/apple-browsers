9e13120eae49dc402b9a167ea711ca3e038abceb
Fernando Bunn <bunn@idevzilla.com>
2025-04-02 18:27:22 +0100

    Update tests

--- macOS/UnitTests/Menus/MainMenuTests.swift
+++ macOS/UnitTests/Menus/MainMenuTests.swift
@@ -241,7 +241,7 @@ class MainMenuTests: XCTestCase {
         // GIVEN
         let aiChatConfig = DummyAIChatConfig()
         aiChatConfig.shouldDisplayApplicationMenuShortcut = true
-        aiChatConfig.isFeatureEnabledForApplicationMenuShortcut = true
+        aiChatConfig.shouldDisplayAddressBarShortcut = true
 
         let sut = MainMenu(featureFlagger: DummyFeatureFlagger(),
                            bookmarkManager: MockBookmarkManager(),
@@ -280,9 +280,7 @@ private class DummyFeatureFlagger: FeatureFlagger {
 
 private class DummyAIChatConfig: AIChatMenuVisibilityConfigurable {
     var shouldDisplayApplicationMenuShortcut = false
-    var shouldDisplayToolbarShortcut = false
-    var isFeatureEnabledForApplicationMenuShortcut = false
-    var isFeatureEnabledForToolbarShortcut = false
+    var shouldDisplayAddressBarShortcut = false
 
     var valuesChangedPublisher: PassthroughSubject<Void, Never> {
         return PassthroughSubject<Void, Never>()
