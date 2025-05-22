9f258c833499be16fa10f354dcd15eb029d0840d
Mariusz Śpiewak <mspiewak@duckduckgo.com>
2025-05-22 21:32:16 +0200

    Improve adding child controller

--- iOS/DuckDuckGo/MainViewController.swift
+++ iOS/DuckDuckGo/MainViewController.swift
@@ -511,12 +511,12 @@ class MainViewController: UIViewController {
         let controller: TabsBarViewController = storyboard.instantiateViewController(identifier: "TabsBar") { coder in
             TabsBarViewController(coder: coder, featureFlagger: self.featureFlagger)
         }
-        controller.willMove(toParent: self)
+        addChild(controller)
         controller.view.frame = viewCoordinator.tabBarContainer.bounds
         controller.delegate = self
         viewCoordinator.tabBarContainer.addSubview(controller.view)
         tabsBarController = controller
-        addChild(controller)
+        controller.didMove(toParent: self)
     }
 
     func startAddFavoriteFlow() {
