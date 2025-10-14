bbd38128a36e76ebe6e7c6a08af6d4b047a86265
Jacek Łyp <jlyp@duckduckgo.com>
2025-10-14 15:48:51 +0200

    Remove unused code

--- iOS/Core/FileStore.swift
+++ iOS/Core/FileStore.swift
@@ -50,21 +50,6 @@ public class FileStore {
         }
     }
 
-    func removeData(forFile file: String) -> Bool {
-        var fileUrl = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: groupIdentifier)
-        fileUrl = fileUrl!.appendingPathComponent(file)
-        guard let fileUrl = fileUrl else { return false }
-        guard FileManager.default.fileExists(atPath: fileUrl.path) else { return true }
-
-        do {
-            try FileManager.default.removeItem(at: fileUrl)
-        } catch {
-            return false
-        }
-
-        return true
-    }
-
     public func loadAsString(for configuration: Configuration) -> String? {
         try? String(contentsOf: persistenceLocation(for: configuration))
     }
