1130ab930c1a89b8fc1023dbf85abfe6feb7a7dc
Tomas Strba <tom@duckduckgo.com>
2025-11-07 13:11:23 +0100

    Revert "Temporarily remove TranslationFrameworkTranslationSource.swift to export PoC"
    This reverts commit 42d5783e9dcffbdb350f94d7bb23c0992ecbf2f5.

--- macOS/DuckDuckGo/Translations/TranslationCoordinator.swift
+++ macOS/DuckDuckGo/Translations/TranslationCoordinator.swift
@@ -42,6 +42,18 @@ final class TranslationCoordinator {
             availableTranslationSources.append(duckDuckGoSource)
         }
 
+        // Add Translation Framework source if available (macOS 26+)
+        if #available(macOS 26.0, *) {
+            let translationFrameworkSource = TranslationFrameworkTranslationSource()
+            if translationFrameworkSource.isAvailable {
+                availableTranslationSources.append(translationFrameworkSource)
+            }
+
+            // FoundationModels source is available but not shown in dropdown
+            // The implementation remains available for potential future use
+            // let foundationModelsSource = FoundationModelsTranslationSource()
+        }
+
         // Set the first available source as current (DuckDuckGo Translation as default)
         currentTranslationSource = availableTranslationSources.first
 
