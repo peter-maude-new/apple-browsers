4941affc2350be70c9b96f36612cf0a245a6b18d
hassaanelgarem <helgarem@duckduckgo.com>
2025-12-02 13:48:23 +0200

    [iOS] Add feature flag for fire button improvements (#2730)
    Task/Issue URL:
    https://app.asana.com/1/137249556945/task/1212116055749740?focus=true
    
    ### Description
    Adds a new feature flag that will be used in future PRs.
    
    ### Testing Steps
    1. Open the debug screen and ensure the new flag
    `granularFireButtonOptions` is added and is overridable.
    
    ### Impact and Risks
    None. 
    
    #### What could go wrong?
    N/A
    
    ### Quality Considerations
    N/A
    
    ---
    ###### Internal references:
    [Definition of
    Done](https://app.asana.com/0/1202500774821704/1207634633537039/f) |
    [Engineering
    Expectations](https://app.asana.com/0/59792373528535/199064865822552) |
    [Tech Design
    Template](https://app.asana.com/0/59792373528535/184709971311943)
    
    <!-- CURSOR_SUMMARY -->
    ---
    
    > [!NOTE]
    > Introduces `granularFireButtonOptions` feature flag (disabled by
    default, locally overridable) and corresponding iOS privacy subfeature.
    > 
    > - **iOS Feature Flags**:
    > - Add `FeatureFlag.granularFireButtonOptions` with source `.disabled`
    and local overriding enabled.
    > - **Privacy Config**:
    > - Add `iOSBrowserConfigSubfeature.granularFireButtonOptions` to
    `PrivacyFeature.iOSBrowserConfig`.
    > 
    > <sup>Written by [Cursor
    Bugbot](https://cursor.com/dashboard?tab=bugbot) for commit
    c3a8872a147db521ccc43be2801817606989e14c. This will update automatically
    on new commits. Configure
    [here](https://cursor.com/dashboard?tab=bugbot).</sup>
    <!-- /CURSOR_SUMMARY -->

--- SharedPackages/BrowserServicesKit/Sources/BrowserServicesKit/PrivacyConfig/Features/PrivacyFeature.swift
+++ SharedPackages/BrowserServicesKit/Sources/BrowserServicesKit/PrivacyConfig/Features/PrivacyFeature.swift
@@ -167,6 +167,9 @@ public enum iOSBrowserConfigSubfeature: String, PrivacySubfeature {
 
     /// https://app.asana.com/1/137249556945/project/72649045549333/task/1211660503405838?focus=true
     case forgetAllInSettings
+
+    /// https://app.asana.com/1/137249556945/project/1211834678943996/task/1212229431540900
+    case granularFireButtonOptions
 }
 
 public enum TabManagerSubfeature: String, PrivacySubfeature {
