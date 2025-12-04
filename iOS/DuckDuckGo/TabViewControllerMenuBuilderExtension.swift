4dd65e81072566eddffff3df1cdf5289cc4e9db9
Mariusz Śpiewak <mspiewak@duckduckgo.com>
2025-12-04 11:41:51 +0100

    Use proper icon for protected state

--- iOS/DuckDuckGo/TabViewControllerMenuBuilderExtension.swift
+++ iOS/DuckDuckGo/TabViewControllerMenuBuilderExtension.swift
@@ -680,7 +680,7 @@ extension TabViewController {
         if useSmallIcon {
             image = isProtected ? DesignSystemImages.Glyphs.Size16.shieldBlocked : DesignSystemImages.Glyphs.Size16.shield
         } else {
-            image = DesignSystemImages.Glyphs.Size24.shield
+            image = isProtected ? DesignSystemImages.Glyphs.Size24.shieldBlocked : DesignSystemImages.Glyphs.Size24.shield
         }
 
         return BrowsingMenuEntry.regular(name: title, image: image, action: { [weak self] in
