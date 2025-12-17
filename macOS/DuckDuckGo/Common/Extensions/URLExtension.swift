8ada4f733d1cd5e22904f3c6d746ff12d676e89d
Juan Manuel Pereira <juanmanuel.pereira1@gmail.com>
2025-12-17 07:31:53 -0300

    Use dev instance

--- macOS/DuckDuckGo/Common/Extensions/URLExtension.swift
+++ macOS/DuckDuckGo/Common/Extensions/URLExtension.swift
@@ -431,7 +431,7 @@ extension URL {
     }
 
     static var duckDuckGo: URL {
-        let duckDuckGoUrlString = "https://duckduckgo.com/"
+        let duckDuckGoUrlString = "https://achawdhary.duckduckgo.com/"
         return URL(string: duckDuckGoUrlString)!
     }
 
