c78edbbb57e3393e635c115009a8622e1cab51ea
Sam Symons <sam@samsymons.com>
2025-12-26 18:55:10 -0800

    Move dependencies to Core.
    Reset project file changes.
    
    Reapply the changes.

---
+++ iOS/Core/Imports.swift
@@ -0,0 +1,32 @@
+//
+//  Imports.swift
+//  DuckDuckGo
+//
+//  Copyright © 2025 DuckDuckGo. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+@_exported import AIChat
+@_exported import BrowserServicesKit
+@_exported import Common
+@_exported import DataBrokerProtection_iOS
+@_exported import Networking
+@_exported import Onboarding
+@_exported import Persistence
+@_exported import PixelKit
+@_exported import PrivacyConfig
+@_exported import SERPSettings
+@_exported import Subscription
+@_exported import VPN
+@_exported import VPNWidgetSupport
