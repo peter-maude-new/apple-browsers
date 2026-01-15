c451dbbe41d167e6475ac865a7da5cf865761dbe
Dominik Kapusta <dkapusta@duckduckgo.com>
2026-01-15 12:43:58 +0100

    Add PrivacyConfig as dependency of VPN package

--- SharedPackages/VPN/Package.swift
+++ SharedPackages/VPN/Package.swift
@@ -24,7 +24,8 @@ let package = Package(
                 .product(name: "Common", package: "BrowserServicesKit"),
                 .product(name: "Networking", package: "BrowserServicesKit"),
                 .product(name: "Subscription", package: "BrowserServicesKit"),
-                .product(name: "PixelKit", package: "BrowserServicesKit")
+                .product(name: "PixelKit", package: "BrowserServicesKit"),
+                .product(name: "PrivacyConfig", package: "BrowserServicesKit")
             ],
             swiftSettings: [
                 .define("DEBUG", .when(configuration: .debug))
